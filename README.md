424236df9540032f19101ed99210634ddb2cadf4
Ishan <koradiaishan335@gmail.com>
2023-10-27 11:15:55 +0530

    dyanmically pick up version of the dashboard pipeline from env var

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dbt-superset-usage
 This repo is used to compute superset usage metrics
+
+
+# Run dbt by passing an env variable that defines the version
+```sh
+export DBT_USAGE_VER=v1 && dbt run
+```
+
--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,9 +1,9 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%} {{ default_schema }}
+    {%- if custom_schema_name is none -%} {{ default_schema }}_{{env_var('DBT_USAGE_VER') }}
 
-    {%- else -%} {{ custom_schema_name }}
+    {%- else -%} {{ custom_schema_name }}_{{env_var('DBT_USAGE_VER')}}
 
     {%- endif -%}
 
