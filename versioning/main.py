590a0b6e5f3967e67ff16c18c1dc1d7313a1b0a9
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-10-27 05:54:04 +0530

    added argparse

--- versioning/main.py
+++ versioning/main.py
@@ -13,23 +13,28 @@ This means
    or keep the same uuid if we want to overwrite the current dashboard
 """
 import os
-import yaml
 from pathlib import Path
 import uuid
 from logging import basicConfig, getLogger, INFO
 import glob
-import shutil
+import argparse
+import yaml
 
 basicConfig(level=INFO)
 logger = getLogger()
 
-export_dir = 'dashboard_export_20231026T182233'
+parser = argparse.ArgumentParser()
+parser.add_argument("export_dir", help="directory containing the exported dashboard")
+parser.add_argument("--upgrade", action="store_true", help="upgrade the dashboard")
+args = parser.parse_args()
+
+export_dir = args.export_dir  # "dashboard_export_20231026T182233"
 
 dataset_dir = Path(export_dir) / "datasets"
-charts_dir =  Path(export_dir) / "charts"
+charts_dir = Path(export_dir) / "charts"
 dashboard_dir = Path(export_dir) / "dashboards"
 
-upgrade = True
+upgrade = args.upgrade
 
 # there will only be one dashboard
 dashboard = None
@@ -37,8 +42,9 @@ for filename in os.listdir(dashboard_dir):
     if filename.endswith(".yaml"):
         with open(dashboard_dir / filename, "r", encoding="utf-8") as dashboard_file:
             dashboard = yaml.safe_load(dashboard_file)
-            dashboard['file_path'] = dashboard_dir / filename
+            dashboard["file_path"] = dashboard_dir / filename
             break
+# pylint:disable=logging-fstring-interpolation
 logger.info(f"read the {dashboard['dashboard_title']} dashhboard")
 
 charts = []
@@ -47,16 +53,16 @@ for filename in os.listdir(charts_dir):
         print(filename)
         with open(charts_dir / filename, "r", encoding="utf-8") as chart_file:
             chart = yaml.safe_load(chart_file)
-            chart['file_path'] = charts_dir / filename
+            chart["file_path"] = charts_dir / filename
             charts.append(chart)
 logger.info("read/loaded all charts")
 
 datasets = []
-for filename in glob.glob(str(dataset_dir) + '/**/*.yaml', recursive = True):
+for filename in glob.glob(str(dataset_dir) + "/**/*.yaml", recursive=True):
     if filename.endswith(".yaml"):
         with open(filename, "r", encoding="utf-8") as dataset_file:
             dataset = yaml.safe_load(dataset_file)
-            dataset['file_path'] = filename
+            dataset["file_path"] = filename
             datasets.append(dataset)
 logger.info("read/loaded all datasets")
 
@@ -86,13 +92,15 @@ for dataset in datasets:
 
     if upgrade:
         # upgrade the version in schema
-        logger.info(f"upgrading the version of schema {dataset['schema']} to {version+1} ")
+        logger.info(
+            f"upgrading the version of schema {dataset['schema']} to {version+1} "
+        )
         dataset["schema"] = dataset["schema"].replace(f"v{version}", f"v{version+1}")
 
     # overwrite the new dataset
     with open(dataset["file_path"], "w", encoding="utf-8") as dataset_file:
-        del dataset['file_path']
-        yaml.safe_dump(dataset, dataset_file, allow_unicode=True)    
+        del dataset["file_path"]
+        yaml.safe_dump(dataset, dataset_file, allow_unicode=True)
 
 
 # 3. Generate new "uuid" for each chart
@@ -101,15 +109,15 @@ for chart in charts:
 
     # 4. Update the newly generated chart uuid of corresponding charts referenced in the dashboard
     for key, config in dashboard["position"].items():
-        if key.startswith('CHART'):
-            if config['meta']["uuid"] == chart["uuid"]:
-                config['meta']["uuid"] = new_chart_uuid
+        if key.startswith("CHART"):
+            if config["meta"]["uuid"] == chart["uuid"]:
+                config["meta"]["uuid"] = new_chart_uuid
 
     chart["uuid"] = new_chart_uuid
 
     # overwrite the new chart
     with open(chart["file_path"], "w", encoding="utf-8") as chart_file:
-        del chart['file_path']
+        del chart["file_path"]
         yaml.safe_dump(chart, chart_file, allow_unicode=True)
 
 # 5. Update the title of the dashboard
@@ -121,8 +129,8 @@ dashboard["uuid"] = str(uuid.uuid4())
 
 # write the new dashboard
 with open(dashboard["file_path"], "w", encoding="utf-8") as dashboard_file:
-    del dashboard['file_path']
+    del dashboard["file_path"]
     # upgrade the version in title
     if upgrade:
-        dashboard['dashboard_title'] = f"Superset usage v{version+1}.0"
+        dashboard["dashboard_title"] = f"Superset usage v{version+1}.0"
     yaml.safe_dump(dashboard, dashboard_file, allow_unicode=True)
