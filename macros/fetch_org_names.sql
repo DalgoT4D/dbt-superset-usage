8a7ec1fd7f730ce1b5ec4bad69631ba0474cba36
Ishankoradia <ikoradia@umich.edu>
2024-12-21 09:02:39 +0530

    added ummeed org

--- macros/fetch_org_names.sql
+++ macros/fetch_org_names.sql
@@ -1,3 +1,3 @@
 {% macro fetch_org_names() -%}
-    {{ return(['sneha', 'atcef', 'noora_health', 'arghyam', 'stir', 'arogya_world', 'shofco', 'inrem']) }}
+    {{ return(['sneha', 'atcef', 'noora_health', 'arghyam', 'stir', 'arogya_world', 'shofco', 'inrem', 'ummeed']) }}
 {%- endmacro %}
