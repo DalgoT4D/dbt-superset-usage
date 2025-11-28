713d9b8604f0ec00658d3f99051cfad13d422433
Ishankoradia <ikoradia@umich.edu>
2025-11-28 18:13:15 +0530

    updates

--- macros/fetch_org_names.sql
+++ macros/fetch_org_names.sql
@@ -1,3 +1,3 @@
 {% macro fetch_org_names() -%}
-    {{ return(['sneha', 'atcef', 'noora_health', 'arghyam', 'stir', 'shofco', 'inrem', 'ummeed', 'cmhlp', 'tech4dev', 'goonj']) }}
+    {{ return(['sneha', 'atcef', 'noora_health', 'akrsp', 'stir', 'shofco', 'inrem', 'ummeed', 'cmhlp', 'tech4dev', 'goonj']) }}
 {%- endmacro %}
